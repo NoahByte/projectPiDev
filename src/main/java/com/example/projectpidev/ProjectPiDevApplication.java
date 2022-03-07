5d00b5529671386665c0d9cfac831e50aac4e8fe
NoahByte <noah.benjamin@esprit.tn>
2022-03-07 12:51:22 +0100

    first

---
+++ src/main/java/com/example/projectpidev/ProjectPiDevApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectpidev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPiDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectPiDevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projectpidev/ProjectPiDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectpidev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPiDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
